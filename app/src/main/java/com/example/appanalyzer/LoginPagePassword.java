05f16f393543ef48c33b0043e0ed24004aaa1dc0
Aruba <aruba.sid@gmail.com>
2021-04-29 22:32:52 +0530

    Login

--- app/src/main/java/com/example/appanalyzer/LoginPagePassword.java
+++ app/src/main/java/com/example/appanalyzer/LoginPagePassword.java
@@ -1,5 +1,6 @@
 package com.example.appanalyzer;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
@@ -7,20 +8,31 @@ import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPagePassword extends AppCompatActivity {
     private TextView userNameTextView;
     private Toolbar toolbar;
     private TextInputEditText passwordEditText;
     private Button loginButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,12 +62,13 @@ public class LoginPagePassword extends AppCompatActivity {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 //on text changed make the login button clickable
-                if(!charSequence.toString().trim().isEmpty()){
+                if (!charSequence.toString().trim().isEmpty()) {
                     loginButton.setClickable(true);
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         loginButton.setTextColor(getApplication().getColor(R.color.white));
+                        loginUser();
                     }
-                }else{
+                } else {
                     loginButton.setClickable(false);
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         loginButton.setTextColor(getApplication().getColor(R.color.silver));
@@ -70,4 +83,58 @@ public class LoginPagePassword extends AppCompatActivity {
             }
         });
     }
+
+    private void loginUser() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        final String username = userNameTextView.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        auth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    if (auth.getCurrentUser().isEmailVerified()) {
+                        String userEmail = auth.getCurrentUser().getEmail();
+                        Intent intent = new Intent(LoginPagePassword.this, MainActivity.class);
+                        intent.putExtra("EMAIL", userEmail);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(intent);
+                    }
+                    if (!(auth.getCurrentUser().isEmailVerified())) {
+                        Toast.makeText(LoginPagePassword.this, "Please verify your email address", Toast.LENGTH_SHORT).show();
+                        final FirebaseUser user = auth.getCurrentUser();
+                        user.sendEmailVerification().addOnCompleteListener(LoginPagePassword.this, new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginPagePassword.this, "A new verification email has been sent. Please verify your email" + user.getEmail(), Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(LoginPagePassword.this, "Failed to send verification email", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    }
+                }
+            }
+        }). addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    Toast.makeText(LoginPagePassword.this, "Invalid password", Toast.LENGTH_SHORT).show();
+                } else if (e instanceof FirebaseAuthInvalidUserException) {
+                    String errorCode = ((FirebaseAuthInvalidUserException) e).getErrorCode();
+                    if (errorCode.equals("ERROR_USER_NOT_FOUND")) {
+                        Toast.makeText(LoginPagePassword.this, "This email does not belong to a registered account. Please proceed to Sign Up.", Toast.LENGTH_SHORT).show();
+                    } else if (errorCode.equals("ERROR_USER_DISABLED")) {
+                        Toast.makeText(LoginPagePassword.this, "User account has been disabled", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginPagePassword.this, e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+        });
+    }
 }
+
