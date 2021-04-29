4159b8a5549c38e8cffc3978f1eb6e03e8d098b6
badr <badr.basowid@gmail.com>
2021-04-29 18:11:50 +0800

    SignUpPage edit

--- app/src/main/java/com/example/appanalyzer/SignUpPage.java
+++ app/src/main/java/com/example/appanalyzer/SignUpPage.java
@@ -1,10 +1,12 @@
 package com.example.appanalyzer;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
+
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class SignUpPage extends AppCompatActivity {
     private Toolbar toolbar;
@@ -13,6 +15,10 @@ public class SignUpPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
         toolbar = findViewById(R.id.toolbar_sign_up_page);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
 
     }
 }
