af5567e2aa7bae97623aa13818de423ac9f3fc82
Badr-Ba-Sowid <61441185+Badr-Ba-Sowid@users.noreply.github.com>
2021-04-29 15:41:06 +0800

    starter

---
+++ app/src/main/java/com/example/appanalyzer/LoginPageUsername.java
@@ -0,0 +1,24 @@
+package com.example.appanalyzer;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class LoginPageUsername extends AppCompatActivity {
+    private Button signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page_username);
+        signUpButton = findViewById(R.id.sign_up_button_login_page_username);
+
+    }
+}
